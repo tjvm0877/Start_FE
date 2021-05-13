44ca750233e7444d6bd13adb88254d4aa2619969
Hyun <itsmine096@gmail.com>
2021-05-13 16:09:50 +0900

    09 과제 더보기 기능 추가

--- 09/kakao.js
+++ 09/kakao.js
@@ -1,9 +1,12 @@
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
 const $result = document.querySelector('#result');
+const $showMoreBtn = document.querySelector('#showMore-btn');
 
 const url = 'https://dapi.kakao.com/v2/search/web';
 const headers = { Authorization: 'KakaoAK 6b9f484fd295646c5128115d8e9c0243' };
+let apiSize = 10;
+
 function success(data) {
   const { documents } = data;
   const li = documents.map((doc) => {
@@ -21,18 +24,31 @@ function error() {
 
 function search() {
   const { value } = $text;
-
   if ($text.value === '') {
     alert('검색어를 입력해주세요');
     return false;
   }
 
-  fetch(`${url}?query=${value}`, { headers })
+  fetch(`${url}?query=${value}&size=${apiSize}`, { headers })
     .then((res) => res.json())
     .then(success)
     .catch(error);
 
+  apiSize += 10;
+  console.log(apiSize);
   return true;
 }
 
+function reload() {
+  while ($result.hasChildNodes()) {
+    $result.removeChild($result.firstChild);
+  }
+}
+
+function showMore() {
+  reload();
+  search();
+}
+
 $btn.addEventListener('click', search);
+$showMoreBtn.addEventListener('click', showMore);
