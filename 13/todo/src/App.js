c892f42b11ccf01cb9571ce8e9c88004c36311b6
Mr. Remark <itsmine096@gmail.com>
2021-06-17 20:29:16 +0900

    디렉토리 13React 커밋

---
+++ 13/todo/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Header from './Header';
+
+function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(event) {
+    setTodo(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newTodos = [...todos];
+    newTodos.push(todo);
+    setTodos(newTodos);
+    setTodo('');
+  }
+
+  return (
+    <div className="container">
+      <header>
+        <Header />
+        <form className="new-task" onSubmit={handleSubmit}>
+          <input
+            name="text"
+            placeholder="할일을 입력하세요"
+            value={todo}
+            onChange={handleChange}
+          />
+        </form>
+      </header>
+      <div id="result">
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
